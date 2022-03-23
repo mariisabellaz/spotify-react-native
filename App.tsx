3850692fc34f1efabac9004230401f8f7ff86602
mariisabellaz <marilia.isabellaz@gmail.com>
2022-03-22 22:58:37 -0300

    add a theme

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+
+import { ThemeProvider } from 'styled-components';
 import {
   useFonts,
   Montserrat_400Regular,
@@ -8,6 +10,8 @@ import {
   Montserrat_600SemiBold
 } from '@expo-google-fonts/montserrat';
 import AppLoading from 'expo-app-loading';
+
+import theme from './src/theme';
 
 export default function App() {
   const [fontLoaded] = useFonts({
@@ -21,8 +25,8 @@ export default function App() {
       return <AppLoading/>
   }
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
-    </>
+    </ThemeProvider>
   );
 }
---
+++ src/theme/index.ts
@@ -0,0 +1,22 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#F7F7F7',
+
+    PRIMARY_900: '#B83341',
+    PRIMARY_800: '#E03F50',
+    PRIMARY_100: '#D16470',
+    PRIMARY_50: '#FFABB3',
+
+    SHAPE: '#DCDCDC',
+    TITLE: '#FFF',
+
+    GRADIENT: ['#B83341', '#E03F50'],
+  },
+
+  FONTS: {
+    REGULAR: 'Montserrat_400Regular',
+    ITALIC: 'Montserrat_400Regular_Italic',
+    MEDIUM: 'Montserrat_500Medium',
+    BOLD: 'Montserrat_600SemiBold',
+  },
+};
