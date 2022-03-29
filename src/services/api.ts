bddd9e6f9671173c08a52aee3c031d8b31ba8947
mariisabellaz <marilia.isabellaz@gmail.com>
2022-03-29 07:03:28 -0300

    remove token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 // const { TOKEN } = process.env;
-const TOKEN = 'BQANMvvIxTx5_Uha4wV48SN2C6gtwQXK7JvR1CSNAc2XYhcmwd9B82qBV06NIPbJBCvnJJMhsJ22pMpFwXUH7zCcDr03YiHpcoO4ezlJ2PeVWoUqIl5QDBj0Z16Jp3I4VzlAMAyvWbuPHJwQ-RS7PRSrRDtdETHhh0fv9oc'
+const TOKEN = ''
+
 const api = axios.create({
   baseURL: 'https://api.spotify.com/v1',
   headers: {
